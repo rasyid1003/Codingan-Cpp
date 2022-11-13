3cbc68f9c215e1c697f5ff96b16643719ca4383c
Faris Rasyid <85282829+rasyid1003@users.noreply.github.com>
2022-11-13 22:35:07 +0700

    Create programcetaknpm.cpp

---
+++ programcetaknpm.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int angka[8] = {5, 0, 4, 2, 1, 4,8 ,3};
+  //ganti npm masing-masing
+    int i;
+    printf("\n");
+    for (i = 0; i < 8; i++)
+    {
+        printf("%d", angka[i]);
+    }
+    printf("\n");
+}
