9076bbdf896c5e202f0c026c2ae1d3081562d9fb
Faris Rasyid <85282829+rasyid1003@users.noreply.github.com>
2022-11-17 21:17:49 +0700

    Create cetaknpm.cpp

---
+++ cetaknpm.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int angka[8] = {5, 0, 4, 2, 1, 4,8 ,3};
+    int i;
+    printf("\n");
+    for (i = 0; i < 8; i++)
+    {
+        printf("%d", angka[i]);
+    }
+    printf("\n");
+}
